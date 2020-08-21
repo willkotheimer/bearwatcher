d4183afb72ef8146a8550ecbca2a6bb79bf60b86
wkotheimer <wkotheimer@gmail.com>
2020-08-20 19:26:30 -0500

    Added bears array

---
+++ javascripts/helpers/data/bearsArray.js
@@ -0,0 +1,19 @@
+
+const bearsArray = [
+    {
+        name: 'Brown bear',
+        imageUrl: './../../images/brownbear.jpg'
+    },
+    {
+        name: 'Black bear',
+        imageUrl: './../../images/blackbear.jpg'
+    },
+    {
+        name: 'Polar Bear',
+        imageUrl: './../../images/polarbear.jpg'
+    },
+    {
+        name: 'Panda Bear',
+        imageUrl: './../../images/pandabear.jpg'
+    }
+]
